a26e60be36166c05d8a9f62be42b12fdf5029bf9
Tomasz Rudowicz <tomasz@rudowi.cz>
2015-11-20 15:03:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Code goes here
+var App = angular.module('MyApp',[]);
+
+App.controller('FirstController', function($scope, $http)
+{
+  var onUserComplete = function(response)
+  {
+    $scope.user = response.data;
+  }
+  
+  var onError = function(response)
+  {
+    $scope.error = "This is an error. Could not find this user."
+  }
+  
+  $scope.search = function(username)
+  {
+    $http.get("https://api.github.com/users/" + username)
+          .then(onUserComplete, onError);
+  };
+  
+  $scope.sitename = "Welcome to GitHub users wiever!";
+  $scope.username = "angular";
+});
